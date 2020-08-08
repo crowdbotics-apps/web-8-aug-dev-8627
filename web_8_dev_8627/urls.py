c3b3698e22a8d0b01c2f4e219fde87fbf522dcca
Crowdbotics <team@crowdbotics.com>
2020-08-08 04:57:30 +0000

    Update admin title

--- web_8_dev_8627/urls.py
+++ web_8_dev_8627/urls.py
@@ -32,9 +32,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "web 8"
-admin.site.site_title = "web 8 Admin Portal"
-admin.site.index_title = "web 8 Admin"
+admin.site.site_header = "web 8 aug"
+admin.site.site_title = "web 8 aug Admin Portal"
+admin.site.index_title = "web 8 aug Admin"
 
 # swagger
 api_info = openapi.Info(
